ad0a828b4e879bcaa7807062ae0f0506e4ca2c1d
lilplatypus8 <jmschmitz8@gmail.com>
2025-10-05 00:12:51 +0000

    tentatively finished comp languages exam 1

--- Fall-2025/comparative-languages/midterm-code.rkt
+++ Fall-2025/comparative-languages/midterm-code.rkt
@@ -24,9 +24,8 @@
 (define (count-evens lst)
   
   (cond [(empty? lst) 0] ; BASECASE: if lst is empty, return an empty list
-        [(equal? (modulo (car lst) 2) 0) (+ 1 (count-evens (rest lst)))] ; if car of lst is even (divisible by 2), add one to output of recursive call on rest of list
+        [(equal? (modulo (first lst) 2) 0) (+ 1 (count-evens (rest lst)))] ; if car of lst is even (divisible by 2), add one to output of recursive call on rest of list
         [else (count-evens (rest lst))])) ; otherwise, just recursively call count-evens on rest of lst
-
 
 ; Problem 6b
 ; count-evens-nested
@@ -35,12 +34,47 @@
 (define (count-evens-nested lst)
 
   (cond [(empty? lst) 0] ; BASECASE: if lst is empty, return an empty list
-        [(list? (car lst)) (+ (count-evens-nested (car lst)) (count-evens-nested (rest lst)))] ; if car of lst is a sub-list, add results of recursive calls on car and rest of list
-        [(equal? (modulo (car lst) 2) 0) (+ 1 (count-evens-nested (rest lst)))] ; if car of lst is even (divisible by 2), add one to output of recursive call on rest of list
+        [(list? (first lst)) (+ (count-evens-nested (first lst)) (count-evens-nested (rest lst)))] ; if car of lst is a sub-list, add results of recursive calls on car and rest of list
+        [(equal? (modulo (first lst) 2) 0) (+ 1 (count-evens-nested (rest lst)))] ; if car of lst is even (divisible by 2), add one to output of recursive call on rest of list
         [else (count-evens-nested (rest lst))])) ; otherwise, just recursively call count-evens on rest of lst
 
 
-; TEST CASES
+; Problem 9b
+(define-struct car (type model color num-doors top-speed)#:mutable)
+
+(define ford
+  (car "truck" "f-150" "blue" 4 120))
+
+(define honda
+  (car "sedan" "pilot" "red" 2 110))
+
+(define kia 
+  (car "sedan" "soul" "silver" 4 100))
+
+; modifying the last 3 values for all 3 car types
+(set-car-color! ford "purple")
+(set-car-num-doors! ford 2)
+(set-car-top-speed! ford 150)
+(set-car-color! honda "yellow")
+(set-car-num-doors! honda 4)
+(set-car-top-speed! honda 80)
+(set-car-color! kia "green")
+(set-car-num-doors! kia 2)
+(set-car-top-speed! kia 90)
+
+; accessing the last 3 values for all 3 car types
+(car-color ford)
+(car-num-doors ford)
+(car-top-speed ford)
+(car-color honda)
+(car-num-doors honda)
+(car-top-speed honda)
+(car-color kia)
+(car-num-doors kia)
+(car-top-speed kia)
+
+
+; TEST CASES for problems 5 and 6
 (div-by 30)
 (count-evens '(1 4 3 7 8))
 (count-evens-nested '(1 (2 3) (4 (5 6))))
