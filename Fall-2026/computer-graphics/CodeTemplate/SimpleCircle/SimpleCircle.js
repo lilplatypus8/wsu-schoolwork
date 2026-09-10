b421080b3dfaa151605099b8a66822ee771b2fae
lilplatypus8 <jmschmitz8@gmail.com>
2026-09-10 19:40:58 +0000

    finished embedded systems lab 1

--- Fall-2026/computer-graphics/CodeTemplate/SimpleCircle/SimpleCircle.js
+++ Fall-2026/computer-graphics/CodeTemplate/SimpleCircle/SimpleCircle.js
@@ -11,11 +11,48 @@ function drawCircle() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     
     var arrayOfPointsForCircle=[];
-    
+  
+	var thetastart = 0.0;
+	var thetaend = 2.0 * Math.PI;
+	var n = 100;
+	var thetastep = thetaend / n;
+	var i = 0.0;
+	var r = 0.6;
+	var a = -0.1;
+	var b = 0.2;
+	
+	for (i = 0; i < n; i++) {
+		var theta = i * thetastep;
+		var x = a + r * Math.cos(theta);
+		var y = b + r * Math.sin(theta);
+		var p = vec2( x, y );
+		arrayOfPointsForCircle.push(p);
+	}
+	
     // Enter array setup code here
     // Use the implicit form of the circle equation here:
     // (x-a)^2 + (y-b)^2 = r^2.
+	
+	/*var xend = 1.0;
+	var xstart = -1.0;
+	var n = 9;
+	var xstep = (xend - xstart) / (n-1);
+	var r = 1.0;
     
+	for (i = 0; i < n; i++) {
+		var x = xstart + i * xstep;
+		var y = Math.sqrt(r*r - x*x);
+		var p = vec2( x, y );
+		arrayOfPointsForCircle.push(p);
+	}
+	
+	for (i = 0; i < n; i++) {
+		var x = xstart + i * xstep;
+		var y = Math.sqrt(r*r - x*x);
+		var p = vec2( x, -y );
+		arrayOfPointsForCircle.push(p);
+	}*/
+	
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER,
@@ -30,6 +67,6 @@ function drawCircle() {
     gl.enableVertexAttribArray( myPosition );
     
     // Enter draw arrays code here
-    
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, n*2);
 }
 
